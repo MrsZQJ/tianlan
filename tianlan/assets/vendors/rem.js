44678d40d059277f20411648f33724296d57267b
feitaowx <6274032@qq.com>
2019-05-14 09:21:15 +0800

    first commit

--- tianlan/assets/vendors/rem.js
+++ tianlan/assets/vendors/rem.js
@@ -1,20 +1,120 @@
-function getHtmlFontsize() {
-  // 假设一个设计稿的宽度 750(记得改回去)
-  var DesignWidth = 750;
-  var DesignFontSize = 100;
-  // 获取当前视口的宽度
-  var nowWidth = document.documentElement.clientWidth;
-  console.log(nowWidth);
-  
-  /*750 / 200 == 375 / 100
-   设计稿宽度 / 设计稿根元素 == 当前屏幕宽度 / 当前屏幕根元素
-   375 / (750 / 200) == 100
-   当屏幕宽度  / (设计稿宽度 / 设计稿的根元素)
-   320 / (750 / 200)  == 85.3333px */
-  /* 4 / 2 == 2 / x
-   2 / ( 4 / 2 )  == 1*/
-  var nowFontSize = nowWidth / (DesignWidth / DesignFontSize);
-  document.documentElement.style.fontSize = nowFontSize + 'px';
-}
-getHtmlFontsize();
-window.addEventListener('resize', getHtmlFontsize);
+(function (win, lib) {
+  var doc = win.document;
+  var docEl = doc.documentElement;
+  var metaEl = doc.querySelector('meta[name="viewport"]');
+  var flexibleEl = doc.querySelector('meta[name="flexible"]');
+  var dpr = 0;
+  var scale = 0;
+  var tid;
+  var flexible = lib.flexible || (lib.flexible = {});
+
+  if (metaEl) {
+      console.warn('将根据已有的meta标签来设置缩放比例');
+      var match = metaEl.getAttribute('content').match(/initial\-scale=([\d\.]+)/);
+      if (match) {
+          scale = parseFloat(match[1]);
+          dpr = parseInt(1 / scale);
+      }
+  } else if (flexibleEl) {
+      var content = flexibleEl.getAttribute('content');
+      if (content) {
+          var initialDpr = content.match(/initial\-dpr=([\d\.]+)/);
+          var maximumDpr = content.match(/maximum\-dpr=([\d\.]+)/);
+          if (initialDpr) {
+              dpr = parseFloat(initialDpr[1]);
+              scale = parseFloat((1 / dpr).toFixed(2));
+          }
+          if (maximumDpr) {
+              dpr = parseFloat(maximumDpr[1]);
+              scale = parseFloat((1 / dpr).toFixed(2));
+          }
+      }
+  }
+
+  if (!dpr && !scale) {
+      var isAndroid = win.navigator.appVersion.match(/android/gi);
+      var isIPhone = win.navigator.appVersion.match(/iphone/gi);
+      var devicePixelRatio = win.devicePixelRatio;
+      if (isIPhone) {
+          // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
+          if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {
+              dpr = 3;
+          } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)) {
+              dpr = 2;
+          } else {
+              dpr = 1;
+          }
+      } else {
+          // 其他设备下，仍旧使用1倍的方案
+          dpr = 1;
+      }
+      scale = 1 / dpr;
+  }
+
+  docEl.setAttribute('data-dpr', dpr);
+  if (!metaEl) {
+      metaEl = doc.createElement('meta');
+      metaEl.setAttribute('name', 'viewport');
+      metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale +
+          ', minimum-scale=' +
+          scale + ', user-scalable=no');
+      if (docEl.firstElementChild) {
+          docEl.firstElementChild.appendChild(metaEl);
+      } else {
+          var wrap = doc.createElement('div');
+          wrap.appendChild(metaEl);
+          doc.write(wrap.innerHTML);
+      }
+  }
+
+  function refreshRem() {
+      var width = docEl.getBoundingClientRect().width;
+      if (width / dpr > 1300) {
+          width = 1300 * dpr;
+      }
+      var rem = width / 12;
+      docEl.style.fontSize = rem + 'px';
+      flexible.rem = win.rem = rem;
+  }
+
+  win.addEventListener('resize', function () {
+      clearTimeout(tid);
+      tid = setTimeout(refreshRem, 300);
+  }, false);
+  win.addEventListener('pageshow', function (e) {
+      if (e.persisted) {
+          clearTimeout(tid);
+          tid = setTimeout(refreshRem, 300);
+      }
+  }, false);
+
+  if (doc.readyState === 'complete') {
+      doc.body.style.fontSize = 12 * dpr + 'px';
+  } else {
+      doc.addEventListener('DOMContentLoaded', function (e) {
+          doc.body.style.fontSize = 12 * dpr + 'px';
+      }, false);
+  }
+
+
+  refreshRem();
+
+  flexible.dpr = win.dpr = dpr;
+  flexible.refreshRem = refreshRem;
+  flexible.rem2px = function (d) {
+      var val = parseFloat(d) * this.rem;
+      if (typeof d === 'string' && d.match(/rem$/)) {
+          val += 'px';
+      }
+      return val;
+  }
+  flexible.px2rem = function (d) {
+      var val = parseFloat(d) / this.rem;
+      if (typeof d === 'string' && d.match(/px$/)) {
+          val += 'rem';
+      }
+      return val;
+  }
+  alert
+
+})(window, window['lib'] || (window['lib'] = {}));
